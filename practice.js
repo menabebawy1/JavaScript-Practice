c3f01657b7c854729d745a0073c3a6d2ae3903b7
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-02-25 14:52:12 -0500

    justins functions

--- practice.js
+++ practice.js
@@ -1,11 +1,11 @@
 //#2 map()
-Array.prototype.myMap = function(callback){
-    const newArray = [];
-    for(let i = 0; i < this.length; i++){
-        newArray.push(callback(this[i])); //Add the mapped elements to the new array
-    }
-    return newArray;
-}
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i])); //Add the mapped elements to the new array
+  }
+  return newArray;
+};
 
 /*
 //map() Testing
@@ -18,16 +18,15 @@ const result = array1.myMap(by2);
 console.log(result); //outputs: [ 2, 4, 6, 8 ]
 */
 
-
 //#6 reduce()
-Array.prototype.myReduce = function(callback, num = 0){
-    let result = 0;
-    for(let i = 0; i < this.length; i++){
-        result += this[i];
-    }
-    result = callback(result, num); //Add the extra number to our result
-    return result;
-}
+Array.prototype.myReduce = function (callback, num = 0) {
+  let result = 0;
+  for (let i = 0; i < this.length; i++) {
+    result += this[i];
+  }
+  result = callback(result, num); //Add the extra number to our result
+  return result;
+};
 
 /*
 //reduce() Testing
@@ -41,15 +40,16 @@ console.log(array1.myReduce(reducer, 5)); //output: 15
 */
 
 //#10 lastIndexOf()
-Array.prototype.myLastIndexOf = function(target){
-    let index = -1
-    for(let i = this.length; i >= 0; i--){ //Go through the array backwards
-        if(this[i] == target){
-            return i;
-        }
+Array.prototype.myLastIndexOf = function (target) {
+  let index = -1;
+  for (let i = this.length; i >= 0; i--) {
+    //Go through the array backwards
+    if (this[i] == target) {
+      return i;
     }
-    return index;
-}
+  }
+  return index;
+};
 
 /*
 //lastIndexOf() Testing
@@ -71,7 +71,6 @@ function myFilter(arr, length) {
   return temparr;
 }
 
-
 //
 //#7 Array.prototype.includes
 //
@@ -89,7 +88,6 @@ function myIncludes(arr, num) {
   return bool;
 }
 
-
 //
 //#11 Object.keys()
 //
@@ -101,3 +99,49 @@ function grabkeys(obj) {
   }
   return temparr;
 }
+
+let testArray1 = [1, 3, 4];
+let testArray2 = [9, 2, 6];
+
+//#1 - forEach() - start
+Array.prototype.myEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+const testMyEach = function (p) {
+  console.log(p);
+};
+testArray1.myEach(testMyEach);
+//#1 - forEach() - end
+
+//#5 - every() - start
+Array.prototype.myEveryThang = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] !== undefined) {
+      if (callback(this[i], i, this) === false) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const testMyEveryThang = function (p) {
+  return p > 5;
+};
+console.log(testArray1.myEveryThang(testMyEveryThang));
+//#5 - every() - end
+
+//#9 - push() - start
+Array.prototype.myPush = function (arr, elementToAdd) {
+  if (arr.length === 0) {
+    arr[0] = elementToAdd;
+  } else {
+    arr[arr.length] = elementToAdd;
+  }
+  return arr.length;
+};
+
+console.log(testArray1.myPush(testArray2, 7));
+console.log(testArray2.myEach(testMyEach));
+//#9 - push() - end
